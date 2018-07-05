861418d9e1d465ed3b392da473af0a4a99e2f9d1
pnaik93 <pnaik93@gmail.com>
2018-07-05 15:48:17 +0530

    Adding Experience component

---
+++ src/components/experience/Experience.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Experience = () => {
+    return <section id='experience' className='component flex experience '>
+        <div className='component-container'><p><i>I</i> am currently working at Barclays as a Front End Developer.</p>
+            <p>My main responsibilities include:</p>
+            <ul className='responsibilities'>
+                <li>Develop new features for Barclays US website.</li>
+                <li>Understand design layout from UX team and deliver pixel perfect, functional webpage.</li>
+                <li>Create POCs for different Architecture/Technologies to be used in Barclays US.</li>
+                <li>Write readable, reusable &amp; maintainable code.</li>
+                <li>Collaborate with team on implementing new features compatile across browsers.</li>
+                <li>Pair Programming, Agile Methodologies (Scrum), CI/CD, Unit Testing, Code Reviews.</li>
+            </ul>
+        </div>
+    </section>
+};
+
+export default Experience;
---
+++ src/components/main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Experience from '../experience/Experience';
+
+const Main = () => {
+    return <main>
+        <Experience />
+    </main>
+};
+
+export default Main;
