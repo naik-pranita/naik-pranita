6baa826beea3a1bcb44bb5f105e4768b2de5068f
pnaik93 <pnaik93@gmail.com>
2018-07-05 17:24:54 +0530

    opening contact links in a new tab

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,14 +6,15 @@ const Contact = () => {
             <div className='component-container'><h3>Contact Details:</h3>
                 <ul>
                     <li><a href=''><i className='fas fa-phone'/> +918087809466</a></li>
-                    <li><a mailto='naik.pranita25@gmail.com' className='text-uppercase'><i
-                        className='fas fa-envelope' />naik.pranita25@gmail.com</a>
+                    <li><a href='mailto:naik.pranita25@gmail.com' className='text-uppercase'><i
+                        className='fas fa-envelope'/>naik.pranita25@gmail.com</a>
                     </li>
                 </ul>
-                <a href='https://www.linkedin.com/in/pranita-naik'><i className='fab fa-linkedin-in'/></a><a
-                    href='https://github.com/pnaik93'><i className='fab fa-github'/></a><a
-                    href='https://twitter.com/Pranita_Naik_08'><i className='fab fa-twitter'/></a><a
-                    href='https://www.facebook.com/pranita.naik93'><i className='fab fa-facebook-square'/></a></div>
+                <a href='https://www.linkedin.com/in/pranita-naik' target='_blank'><i className='fab fa-linkedin-in'/></a>
+                <a href='https://github.com/pnaik93' target='_blank'><i className='fab fa-github'/></a>
+                <a href='https://twitter.com/Pranita_Naik_08' target='_blank'><i className='fab fa-twitter'/></a>
+                <a href='https://www.facebook.com/pranita.naik93' target='_blank'><i className='fab fa-facebook-square'/></a>
+            </div>
         </section>)
 };
 
